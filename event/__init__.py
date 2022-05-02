08a85bc9a9726ff3ffce00ddb80721832af6ae2d
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-05-02 22:21:15 +0100

    Move Handler into its own module

--- event/__init__.py
+++ event/__init__.py
@@ -1,32 +1,6 @@
-import dataclasses
-import enumb
+from .handler import Handler
 
+HANDLER: Handler = Handler()
 
-class Event(enumb.StrEnum):
-    pass
-
-
-@dataclasses.dataclass
-class Handler:
-    hooks: dict = dataclasses.field(default_factory=dict)
-
-    def on(self, event):
-        def wrapper(func):
-            self.bind(event, func)
-
-            return func
-
-        return wrapper
-
-    def bind(self, event, func):
-        self.hooks.setdefault(event, []).append(func)
-
-    def dispatch(self, event, *args, **kwargs):
-        for hook in self.hooks.get(event, ()):
-            hook(*args, **kwargs)
-
-
-GLOBAL_HANDLER = Handler()
-
-on = GLOBAL_HANDLER.on
-dispatch = GLOBAL_HANDLER.dispatch
+on = HANDLER.on
+dispatch = HANDLER.dispatch
